a41baf06d0b7d7fff12635114fc037ea73a338e0
waylybaye <havelove@gmail.com>
2017-04-16 22:13:21 +0800

    OpenVPN works

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM kylemanna/openvpn
 
 MAINTAINER HyperApp <hyperappcloud@gmail.com>
 
+ENV DEBUG 0
 ENV DOMAIN=
-ENV CLIENT_NAME HyperApp
+ENV CLIENT_NAME HyperAppOVPN
 ENV EASYRSA_BATCH true
 ENV EASYRSA_REQ_CN $DOMAIN
 
-ENTRYPOINT ['docker-entrypoint.sh']
+COPY docker-entrypoint.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["ovpn_run"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-if [ -z ${DOMAIN+x} ]; then
-  echo "[ERROR] Domain is required";
+
+if [ -z $DOMAIN ]; then
+  echo "[ERROR] Domain is required"
   exit 1
 fi
 
-if ! [ -e /etc/openvpn/openvpn.conf ]; then
-  echo "Initializing Configuation ..."
-  ovpn_genconfig -u udp://$DOMAIN;
+if [ -z $EASYRSA_REQ_CN ]; then
+  export EASYRSA_REQ_CN=$DOMAIN
 fi
 
-if ! [ -d /etc/openvpn/pki ]; then
-  echo "Creating certificates ..."
-  ovpn_initpki nopass;
+echo "EASYRSA_REQ_CN: " $EASYRSA_REQ_CN
+
+
+if ! [ -e /etc/openvpn/openvpn.conf ]; then
+  echo "[*] Initializing Configuation ..."
+
+  ovpn_genconfig -u udp://$DOMAIN
+fi
+
+if ! [ -e /etc/openvpn/pki/ca.crt ]; then
+  echo "[*] Creating certificates ..."
+  ovpn_initpki nopass || exit 1
 fi
 
 if ! [ -e /etc/openvpn/pki/private/$CLIENT_NAME.key ]; then
-  echo "Creating clients certificates ..."
-  easyrsa build-client-full $CLIENT_NAME nopass;
+  echo "[*] Creating clients certificates ..."
+  easyrsa build-client-full $CLIENT_NAME nopass
 fi
 
 if ! [ -e /etc/openvpn//$CLIENT_NAME.ovpn ]; then
-  echo "Creating clients configuration ..."
-  easyrsa ovpn_getclient $CLIENT_NAME > /etc/openvpn/$CLIENT_NAME.ovpn;
+  echo "[*] Creating clients configuration ..."
+  ovpn_getclient $CLIENT_NAME > /etc/openvpn/$CLIENT_NAME.ovpn
 fi
 
 exec "$@"
